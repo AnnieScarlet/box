eded59c35d6601000803d4bb42e4d63e5f90811f
Annie Scarlet <take.gn+annie@gmail.com>
2018-01-21 23:17:39 +0900

    rename

---
+++ backend/secret/django_settings.py
@@ -0,0 +1,11 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=!xyfp1n!-3krbpsi1afjnn&!+xf(xk#8itnk-!l5&c$b8g^vb'
+
+AWS_ACCESS_KEY_ID = 'AKIAIDULIA5EGVIIW4AQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'eorzeabox-dev'
+
+DB_HOST = 'localhost'
+DB_NAME = 'eorzeabox'
+DB_USER = 'root'
+DB_PASSWORD = ''
